c0851c4042a7872c7b8b23c8db77ca797ddf92a3
amjadamju <amjadpanjeeri@gmail.com>
2020-12-09 23:50:09 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const { auth } = require('express-openid-connect');
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: 'a long, randomly-generated string stored in env',
+    baseURL: 'http://localhost:3000',
+    clientID: 'AI06OVBozbBrL4NsHqgeseJGzWoMNzDd',
+    issuerBaseURL: 'https://dev-a6gnj6s2.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
