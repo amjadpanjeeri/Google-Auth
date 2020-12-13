8f2ac5eec2a159c5f8756630113d8c374d345844
amjadamju <amjadpanjeeri@gmail.com>
2020-12-13 18:40:56 +0530

    added profile route

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
+const port = process.env.port || 3000;
+
 
 const { auth } = require('express-openid-connect');
+app.use(
+    auth({
+        authRequired: false,
+        auth0Logout: true,
+        issuerBaseURL: process.env.ISSUER_BASE_URL,
+        baseURL: process.env.BASE_URL,
+        clientID: process.env.CLIENT_ID,
+        secret: process.env.SECRET,
+    })
+);
 
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: 'a long, randomly-generated string stored in env',
-    baseURL: 'http://localhost:3000',
-    clientID: 'AI06OVBozbBrL4NsHqgeseJGzWoMNzDd',
-    issuerBaseURL: 'https://dev-a6gnj6s2.us.auth0.com'
-};
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', (req, res) => {
+    const name = req.oidc.user.name;
+    const email = req.oidc.user.email;
+    let accessToken = req.oidc.id_Token;
+    console.log(accessToken);
+    // const name = req.oidc.user.name;
+    // const name = req.oidc.user.name;
+
+    res.json(req.oidc.user);
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
